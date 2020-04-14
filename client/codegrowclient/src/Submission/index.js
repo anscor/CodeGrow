793f34b3d55720d6dcce3e0db557e92220feb609
anscor <xlyanscor@outlook.com>
2020-04-14 19:14:38 +0800

    代码详情展示界面、代码高亮插件

--- client/codegrowclient/src/Submission/index.js
+++ client/codegrowclient/src/Submission/index.js
@@ -2,7 +2,7 @@
  * @Author: Anscor
  * @Date: 2020-04-13 17:56:45
  * @LastEditors: Anscor
- * @LastEditTime: 2020-04-13 21:52:16
+ * @LastEditTime: 2020-04-14 18:41:50
  * @Description: 提交界面
  */
 import React, { useEffect } from "react"
@@ -12,36 +12,49 @@ import * as Actions from '../redux/actions'
 import { Table, Button, Modal } from "antd";
 import { useLocation } from "react-router-dom";
 import { toLocalDate } from "../Top";
+import CodeDetail from "./CodeDetail";
 
-const columns = [
+import './index.css'
+
+const tableColumns = props => [
     { title: "提交ID", dataIndex: "id", key: "id" },
     { title: "提交时间", dataIndex: "submit_time", key: "submit_time" },
     { title: "语言", dataIndex: "language", key: "language" },
     { title: "用时", dataIndex: "time", key: "time" },
     { title: "使用内存", dataIndex: "memory", key: "memory" },
-    { title: "运行结果", dataIndex: "result", key: "result" },
+    {
+        title: "运行结果",
+        dataIndex: "result",
+        key: "result",
+        render: text => {
+            if (text === "Compile Error")
+                return <p style={{ color: "green" }}>{text}</p>;
+            else if (text === "Accepted")
+                return <p style={{ color: "blue" }}>{text}</p>;
+            else return <p style={{ color: "red" }}>{text}</p>;
+        }
+    },
     {
         title: "代码详情",
         dataIndex: "code",
         key: "code",
-        render: () => (
-            <Button type="link">
-                代码详情
-                <Modal>
-
-                </Modal>
-            </Button>
-        )
+        render: (text, record) => (<Button
+            type="link"
+            onClick={() => {
+                props.detailClick(props.submissions.find(submission =>
+                    submission.id === record.id));
+            }}>
+            代码详情
+        </Button>)
     },
     {
         title: "",
         dataIndex: "pre",
         key: "pre",
-        render: () => (
-            <Button type="link">
-                与上一版本对比
-            </Button>
-        )
+        render: () => (<Button
+            type="link">
+            与上一版本对比
+        </Button>)
     }
 ];
 
@@ -57,19 +70,34 @@ const SubmissionUI = props => {
         language: submission.language,
         time: submission.time,
         memory: submission.memory,
-        result: submission.result
+        result: submission.result,
+        code: submission.code
     })) : [];
     return (
-        <Table
-            columns={columns}
-            pagination={10}
-            loading={!props.submissions}
-            dataSource={data} />
+        <div>
+            <Table
+                columns={tableColumns(props)}
+                pagination={10}
+                loading={!props.submissions}
+                dataSource={data} />
+            <Modal
+                onCancel={props.detailClose}
+                maskClosable={true}
+                footer={false}
+                centered
+                width={800}
+                title="代码详情"
+                visible={props.detailVisible}>
+                <CodeDetail submission={props.submission} />
+            </Modal>
+        </div>
     );
 }
 
 const mapStateToProps = state => ({
-    submissions: state.submission.submissions
+    submissions: state.submission.submissions,
+    detailVisible: state.submission.detailVisible,
+    submission: state.submission.submission
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -77,6 +105,17 @@ const mapDispatchToProps = dispatch => ({
         dispatch({
             type: Actions.SUBMISSION_FETCH_SUBMISSIONS,
             id: id
+        });
+    },
+    detailClose: () => {
+        dispatch({
+            type: Actions.SUBMISSION_DETAIL_MODAL_CLOSE
+        });
+    },
+    detailClick: submission => {
+        dispatch({
+            type: Actions.SUBMISSION_DETAIL_CLICK,
+            submission: submission
         });
     }
 });
--- client/codegrowclient/src/Submission/reducer.js
+++ client/codegrowclient/src/Submission/reducer.js
@@ -2,14 +2,16 @@
  * @Author: Anscor
  * @Date: 2020-04-13 19:01:38
  * @LastEditors: Anscor
- * @LastEditTime: 2020-04-13 19:56:23
+ * @LastEditTime: 2020-04-14 17:15:51
  * @Description: 提交模块reducer
  */
 
 import * as Actions from '../redux/actions'
 
 export const initialState = {
-    submissions: undefined
+    submissions: undefined,
+    detailVisible: false,
+    submission: undefined
 };
 
 export default (state = {}, action) => {
@@ -17,6 +19,15 @@ export default (state = {}, action) => {
         case Actions.SUBMISSION_FETCH_SUBMISSIONS_SUCCESS:
             return Object.assign({}, state, {
                 submissions: action.submissions
+            });
+        case Actions.SUBMISSION_DETAIL_MODAL_CLOSE:
+            return Object.assign({}, state, {
+                detailVisible: false
+            });
+        case Actions.SUBMISSION_DETAIL_CLICK:
+            return Object.assign({}, state, {
+                detailVisible: true,
+                submission: action.submission
             });
 
         default:
